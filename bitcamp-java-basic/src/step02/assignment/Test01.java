71b9e6e58de52d1d2ce890118fcf10d499e25eb2
bokjinkim <kbj3182@gmail.com>
2018-03-05 18:58:05 +0900

    2018-03-05

---
+++ bitcamp-java-basic/src/step02/assignment/Test01.java
@@ -0,0 +1,52 @@
+/*
+# 과제(2018-03-05)
+사용자로부터 이름, 국어, 영어, 수학 데이터를 입력
+받고 합계와 평균을 출력하라
+## 실행 결과
+'''
+>java -classpath bin step02.assignment.Test01
+이름? 홍길동
+국어? 100
+영어? 90
+수학? 80
+--------
+홍길동 100 90 80 270 90.0
+
+'''
+*/
+package step02.assignment;
+public class Test01 {
+    public static void main(String[] args) {
+        java.io.InputStream keyboard = System.in;
+        java.util.Scanner keyScan = new java.util.Scanner(keyboard);
+        String name;
+        int lan;
+        int eng;
+        int math;
+        int sum;
+        float aver;
+
+        System.out.print("이름 : ");
+        name = keyScan.nextLine();
+        System.out.print("국어 : ");
+        lan = keyScan.nextInt();
+        System.out.print("영어 : ");
+        eng = keyScan.nextInt();
+        System.out.print("수학 : ");
+        math = keyScan.nextInt();
+        sum = lan + eng + math;
+        aver = sum / 3 ;
+
+        System.out.printf("%s %d %d %d %d %.1f", name, lan, eng, math, sum, aver);
+        
+
+    }
+}
+
+// 키보드 정보를 가져온다.
+
+
+// 키보드에서 값을 꺼내주는 도구를 연결한다.
+
+
+// print()는 문자열을 출력한 후 줄 바꿈을 하지 않는다.
--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -1,10 +1,62 @@
 package bitcamp.java106.pms;
 
+import javax.xml.stream.events.StartDocument;
+
 public class App{
     
     public static void main(String[] args){
-        System.out.printf("Hello, World");
+        java.io.InputStream keyboard = System.in;
+        java.util.Scanner keyScan = new java.util.Scanner(keyboard);
+        String team;
+        String explan;
+        int people;
+        String start;
+        String end;
+
+        System.out.print("팀명 : ");
+        team = keyScan.nextLine();
+        System.out.print("설명 : ");
+        explan = keyScan.nextLine();
+        System.out.print("최대인원 : ");
+        people = keyScan.nextInt();
+        keyScan.nextLine();
+        System.out.print("시작일 : ");
+        start = keyScan.nextLine();
+        System.out.print("종료일 : ");
+        end = keyScan.nextLine();
+        
+        System.out.println("---------------------------");
+        System.out.printf("팀명 : %s\n", team);
+        System.out.printf("설명 : \n%s\n", explan);
+        System.out.printf("최대인원 : %d명\n", people);
+        System.out.printf("시작일 : %s\n", start);
+        System.out.printf("종료일 : %s\n", end);
 
     }
 
 }
+/*
+# 자바 프로젝트 
+
+## src02 - 키보드 입력 과 변수
+- 학습목표
+  - 키보드로부터 데이터를 입력 받는 방법을 익힌다.
+  - 사용자가 입력한 값을 메모리에 보관하는 방법을 익힌다.
+- 작업내용
+  - 사용자로부터 팀 정보를 입력 받아 출력하라.
+- 실행 결과
+'''
+> java -classpath bin bitcamp.java106.pms.App
+팀명? 비트비트
+설명? 자바 프로젝트 팀
+최대인원? 5
+시작일? 2018-05-05
+종료일? 2018-08-20
+------------------------
+팀명: 비트비트
+설명:
+자바 프로젝트 팀
+최대인원: 5명
+일자: 2018-05-05 ~ 2018-08-20
+
+*/
