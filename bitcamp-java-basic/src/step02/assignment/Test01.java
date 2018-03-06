5a5d1757147cb82f584f7d7052ddef2dc222e118
bokjinkim <kbj3182@gmail.com>
2018-03-06 17:56:56 +0900

    2018-03-06

--- bitcamp-java-basic/src/step02/assignment/Test01.java
+++ bitcamp-java-basic/src/step02/assignment/Test01.java
@@ -34,8 +34,10 @@ public class Test01 {
         eng = keyScan.nextInt();
         System.out.print("수학 : ");
         math = keyScan.nextInt();
+
+        System.out.println("-------------");
         sum = lan + eng + math;
-        aver = sum / 3 ;
+        aver = sum / 3f ;
 
         System.out.printf("%s %d %d %d %d %.1f", name, lan, eng, math, sum, aver);
         
@@ -50,3 +52,43 @@ public class Test01 {
 
 
 // print()는 문자열을 출력한 후 줄 바꿈을 하지 않는다.
+
+/*
+public class Test01 {
+    public static void main(String[] args) {
+        java.io.InputStream keyboard = System.in;
+        java.util.Scanner keyScan = new java.util.Scanner(keyboard);
+
+        System.out.print("이름? ");
+        String name = keyScan.nextLine();
+
+        System.out.print("국어? ");
+        int kor = keyScan.nextInt();
+
+        System.out.print("영어? ");
+        // 이전에 국어 점수를 입력받을 때 들어온 줄바꿈 코드는 
+        // nextInt() 메서드에서 버린다.
+        int eng = keyScan.nextInt();
+
+        System.out.print("수학? ");
+        int math = keyScan.nextInt();
+
+        int sum = kor + eng + math;
+        float average = sum / 3;
+
+        System.out.println("------------------------------");
+        System.out.print(name);
+        System.out.print(" ");
+        System.out.print(kor);
+        System.out.print(" ");
+        System.out.print(eng);
+        System.out.print(" ");
+        System.out.print(math);
+        System.out.print(" ");
+        System.out.print(sum);
+        System.out.print(" ");
+        System.out.print(average);
+        System.out.println();
+    }
+}
+*/
--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -29,8 +29,8 @@ public class App{
         System.out.printf("팀명 : %s\n", team);
         System.out.printf("설명 : \n%s\n", explan);
         System.out.printf("최대인원 : %d명\n", people);
-        System.out.printf("시작일 : %s\n", start);
-        System.out.printf("종료일 : %s\n", end);
+        System.out.printf("일자 : %s ~ %s", start, end);
+        
 
     }
 
@@ -59,4 +59,40 @@ public class App{
 최대인원: 5명
 일자: 2018-05-05 ~ 2018-08-20
 
+public class App {
+    public static void main(String[] args) {
+        java.io.InputStream keyboard = System.in;
+        java.util.Scanner keyScan = new java.util.Scanner(keyboard);
+
+        System.out.print("팀명? ");
+        String teamName = keyScan.nextLine();
+
+        System.out.print("설명? ");
+        String description = keyScan.nextLine();
+
+        System.out.print("최대인원? ");
+        int maxQty = keyScan.nextInt();
+        keyScan.nextLine(); // 숫자 뒤에 줄바꿈 코드를 읽는다.
+                            // 읽고 난 뒤에 아무것도 안하기 때문에 
+                            // 일종의 줄바꿈 코드를 제거하는 효과가 있다.
+
+        System.out.print("시작일? ");
+        String startDate = keyScan.nextLine();
+
+        System.out.print("종료일? ");
+        String endDate = keyScan.nextLine();
+
+        System.out.println("-----------------------------");
+        System.out.print("팀명: ");
+        System.out.println(teamName);
+        System.out.println("설명: ");
+        System.out.println(description);
+        System.out.print("최대인원: ");
+        System.out.println(maxQty);
+        System.out.print("일자: ");
+        System.out.print(startDate);
+        System.out.print(" ~ ");
+        System.out.println(endDate);
+    }
+}
 */
