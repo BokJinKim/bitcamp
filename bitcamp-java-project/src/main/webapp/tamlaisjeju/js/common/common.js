4381987439ea1b10bae9934b4d932a7c121030da
kimbokjin <kbj3182@gmail.com>
2018-08-16 17:16:21 +0900

    bok

--- bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
+++ bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
@@ -1,6 +1,6 @@
 //var serverRoot = "http://localhost:8888/bitcamp-java-project";
 // var serverRoot = "http://tamlaisjeju.java106.com:8888/bitcamp-java-project";
-var serverRoot = "http://www.kimbokjin.com:8888/bitcamp-java-project";
+var serverRoot = "http://www.kimbokjin.com:8888/pms";
 
 
 
